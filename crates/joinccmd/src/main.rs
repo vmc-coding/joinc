fc1f2db8ad6fdc3404d78ea63a312a77f863782a
vmc <vmc.coding@gmail.com>
2024-02-09 19:27:30 +0100

    cli: Formatting again ..

--- crates/joinccmd/src/main.rs
+++ crates/joinccmd/src/main.rs
@@ -84,12 +84,7 @@ fn main() {
 fn process_command(connection: &mut connection::Connection, command: CliCommand) -> Result<()> {
     match command {
         CliCommand::ClientVersion => {
-            println!(
-                "Client version: {}",
-                ExchangeVersionsCommand::default()
-                    .execute(connection)?
-                    .display()
-            );
+            println!("Client version: {}", ExchangeVersionsCommand::default().execute(connection)?.display());
         }
         CliCommand::GetMessages { seqno } => {
             for msg in GetMessagesCommand::new(seqno).execute(connection)? {
@@ -174,9 +169,7 @@ impl FormattedTimestamp {
 
 impl fmt::Display for FormattedTimestamp {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
+        write!(f, "{}",
             &Some(self.timestamp)
                 .filter(|&t| t > 0.)
                 .and_then(|t| NaiveDateTime::from_timestamp_opt(t as i64, 0))
@@ -207,19 +200,14 @@ impl fmt::Display for Displayable<Error> {
             Error::Deserialization(serde_err) => write!(f, "Deserialization error: {}.", serde_err),
             Error::Io(io_err) => write!(f, "IO error: {}.", io_err),
             Error::Rpc(rpc_err) => write!(f, "RPC error: {}.", rpc_err),
-            Error::Unauthorized => write!(
-                f,
-                "Unauthorized, please set the password via --passwd <PASSWD>."
-            ),
+            Error::Unauthorized => write!(f, "Unauthorized, please set the password via --passwd <PASSWD>."),
         }
     }
 }
 
 impl fmt::Display for Displayable<Message> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}: {} ({}) [{}] {}",
+        write!(f, "{}: {} ({}) [{}] {}",
             self.0.seqno,
             FormattedTimestamp::with_format(self.0.timestamp, "%d-%b-%Y %H:%M:%S"),
             self.0.priority,
@@ -286,35 +274,20 @@ impl fmt::Display for Displayable<&Task> {
         let active_task_state = task
             .active_task
             .as_ref()
-            .map_or(ActiveTaskState::Uninitialized, |at| {
-                at.active_task_state.clone()
-            });
+            .map_or(ActiveTaskState::Uninitialized, |at| at.active_task_state.clone());
 
         writeln!(f, "{INDENT}name: {}", task.name)?;
         writeln!(f, "{INDENT}WU name: {}", task.wu_name)?;
         writeln!(f, "{INDENT}project URL: {}", task.project_url)?;
-        writeln!(
-            f,
-            "{INDENT}received: {}",
-            FormattedTimestamp::new(task.received_time)
-        )?;
-        writeln!(
-            f,
-            "{INDENT}report deadline: {}",
-            FormattedTimestamp::new(task.report_deadline)
-        )?;
+        writeln!(f, "{INDENT}received: {}", FormattedTimestamp::new(task.received_time))?;
+        writeln!(f, "{INDENT}report deadline: {}", FormattedTimestamp::new(task.report_deadline))?;
         writeln!(f, "{INDENT}ready to report: {}", task.ready_to_report)?;
         writeln!(f, "{INDENT}state: {}", task.state)?;
         writeln!(f, "{INDENT}scheduler state: {}", scheduler_state)?;
         writeln!(f, "{INDENT}active_task_state: {}", active_task_state)?;
         writeln!(f, "{INDENT}app version num: {}", task.version_num)?;
-        writeln!(
-            f,
-            "{INDENT}resources: {}",
-            Some(task.resources.as_str())
-                .filter(|rs| !rs.is_empty())
-                .get_or_insert("1 CPU")
-        )?;
+        writeln!(f, "{INDENT}resources: {}",
+            Some(task.resources.as_str()).filter(|rs| !rs.is_empty()).get_or_insert("1 CPU"))?;
 
         if task.state as isize >= 0
             && task.state as isize <= ResultClientState::FilesDownloaded as isize
@@ -322,43 +295,21 @@ impl fmt::Display for Displayable<&Task> {
             if task.suspended_via_gui.into() {
                 writeln!(f, "{INDENT}suspended via GUI: yes")?;
             }
-            writeln!(
-                f,
-                "{INDENT}estimated CPU time remaining: {:.6}",
-                task.estimated_cpu_time_remaining
-            )?;
+            writeln!(f, "{INDENT}estimated CPU time remaining: {:.6}",
+                task.estimated_cpu_time_remaining)?;
         }
 
         if scheduler_state as isize > SchedulerState::Uninitialized as isize {
             if let Some(active_task) = &task.active_task {
-                writeln!(
-                    f,
-                    "{INDENT}CPU time at last checkpoint: {:.6}",
-                    active_task.checkpoint_cpu_time
-                )?;
-                writeln!(
-                    f,
-                    "{INDENT}current CPU time: {:.6}",
-                    active_task.current_cpu_time
-                )?;
+                writeln!(f, "{INDENT}CPU time at last checkpoint: {:.6}", active_task.checkpoint_cpu_time)?;
+                writeln!(f, "{INDENT}current CPU time: {:.6}", active_task.current_cpu_time)?;
                 writeln!(f, "{INDENT}fraction done: {:.6}", active_task.fraction_done)?;
-                writeln!(
-                    f,
-                    "{INDENT}swap size: {:.0} MB",
-                    to_mibi(active_task.swap_size)
-                )?;
-                writeln!(
-                    f,
-                    "{INDENT}working set size: {:.0} MB",
-                    to_mibi(active_task.working_set_size_smoothed)
-                )?;
+                writeln!(f, "{INDENT}swap size: {:.0} MB", to_mibi(active_task.swap_size))?;
+                writeln!(f, "{INDENT}working set size: {:.0} MB", to_mibi(active_task.working_set_size_smoothed))?;
 
                 if active_task.bytes_sent > 0. || active_task.bytes_received > 0. {
-                    writeln!(
-                        f,
-                        "{INDENT}bytes sent: {:.0} received: {:.0}",
-                        active_task.bytes_sent, active_task.bytes_received
-                    )?;
+                    writeln!(f, "{INDENT}bytes sent: {:.0} received: {:.0}",
+                        active_task.bytes_sent, active_task.bytes_received)?;
                 }
             }
         }
