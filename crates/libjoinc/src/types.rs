626806cdcd19419e613b8edd4f3357ada3821ee8
vmc <vmc.coding@gmail.com>
2024-02-10 20:56:34 +0100

    lib: Distinguish floats, durations and timestamps

--- crates/libjoinc/src/types.rs
+++ crates/libjoinc/src/types.rs
@@ -53,30 +53,43 @@ pub struct CCStatus {
     pub network_status: NetworkStatus,
 }
 
+#[derive(Copy, Clone, Debug, Default, Deserialize)]
+#[serde(default)]
+#[serde(transparent)]
+pub struct Duration(pub f64); // in seconds
+
+impl From<Duration> for f64 {
+    fn from(d: Duration) -> Self {
+        d.0
+    }
+}
+
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct FileTransfer {
     pub sticky: Bool,
 
+    pub project_backoff: Duration,
+
     pub nbytes: f64,
-    pub project_backoff: f64, // in seconds
     pub max_nbytes: f64,
 
     pub status: i32,
 
+    pub file_xfer: Option<FileXfer>,
+    pub persistent_file_xfer: Option<PersistentFileXfer>,
+
     pub name: String,
     pub project_name: String,
     pub project_url: String,
-
-    pub file_xfer: Option<FileXfer>,
-    pub persistent_file_xfer: Option<PersistentFileXfer>,
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct FileXfer {
+    pub estimated_xfer_time_remaining: Duration,
+
     pub bytes_xferred: f64,
-    pub estimated_xfer_time_remaining: f64,
     pub xfer_speed: f64,
     pub file_offset: f64,
 
@@ -116,14 +129,13 @@ pub struct Message {
     pub project: String,
     pub seqno: i32,
     #[serde(rename = "time")]
-    pub timestamp: f64,
+    pub timestamp: Timestamp,
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct Notice {
-    pub arrival_time: f64,
-    pub create_time: f64,
+    pub is_private: Bool,
 
     pub seqno: i32,
 
@@ -133,7 +145,8 @@ pub struct Notice {
     pub project_name: String,
     pub title: String,
 
-    pub is_private: Bool,
+    pub arrival_time: Timestamp,
+    pub create_time: Timestamp,
 }
 
 #[derive(Debug, Default, Deserialize)]
@@ -141,12 +154,14 @@ pub struct Notice {
 pub struct PersistentFileXfer {
     pub is_upload: Bool,
 
-    pub first_request_time: f64,
+    pub time_so_far: Duration,
+
     pub last_bytes_xferred: f64,
-    pub next_request_time: f64,
-    pub time_so_far: f64,
 
     pub num_retries: i32,
+
+    pub first_request_time: Timestamp,
+    pub next_request_time: Timestamp,
 }
 
 #[derive(Debug, Default, Deserialize)]
@@ -163,6 +178,9 @@ pub struct Project {
     pub suspended_via_gui: Bool,
     pub trickle_up_pending: Bool,
 
+    pub download_backoff: Duration,
+    pub upload_backoff: Duration,
+
     pub disk_usage: f64,
     pub duration_correction_factor: f64,
     pub elapsed_time: f64,
@@ -173,11 +191,15 @@ pub struct Project {
     pub user_expavg_credit: f64,
     pub user_total_credit: f64,
 
+    pub gui_urls: GuiUrls,
+
     pub hostid: i32,
     pub master_fetch_failures: i32,
     pub njobs_error: i32,
     pub njobs_success: i32,
     pub nrpc_failures: i32,
+
+    pub sched_rpc_pending: RpcReason,
 
     pub external_cpid: String,
     pub master_url: String,
@@ -187,15 +209,9 @@ pub struct Project {
     pub user_name: String,
     pub venue: String,
 
-    // TODO Use a suitable type from std::time
-    pub download_backoff: f64,
-    pub last_rpc_time: f64,
-    pub min_rpc_time: f64,
-    pub project_files_downloaded_time: f64,
-    pub upload_backoff: f64,
-
-    pub sched_rpc_pending: RpcReason,
-    pub gui_urls: GuiUrls,
+    pub last_rpc_time: Timestamp,
+    pub min_rpc_time: Timestamp,
+    pub project_files_downloaded_time: Timestamp,
 }
 
 // We're calling BOINC's 'result' structure 'task' because of
@@ -203,8 +219,6 @@ pub struct Project {
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct Task {
-    pub state: ResultClientState,
-
     pub coproc_missing: Bool,
     pub got_server_ack: Bool,
     pub network_wait: Bool,
@@ -214,15 +228,17 @@ pub struct Task {
     pub scheduler_wait: Bool,
     pub suspended_via_gui: Bool,
 
-    pub estimated_cpu_time_remaining: f64,
-    pub final_cpu_time: f64,
-    pub final_elapsed_time: f64,
-    pub received_time: f64,
-    pub report_deadline: f64,
+    pub estimated_cpu_time_remaining: Duration,
+    pub final_cpu_time: Duration,
+    pub final_elapsed_time: Duration,
 
     pub exit_status: i32,
     pub signal: i32,
     pub version_num: i32,
+
+    pub active_task: Option<ActiveTask>,
+
+    pub state: ResultClientState,
 
     pub name: String,
     pub plan_class: String,
@@ -232,7 +248,19 @@ pub struct Task {
     pub scheduler_wait_reason: String,
     pub wu_name: String,
 
-    pub active_task: Option<ActiveTask>,
+    pub received_time: Timestamp,
+    pub report_deadline: Timestamp,
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize)]
+#[serde(default)]
+#[serde(transparent)]
+pub struct Timestamp(pub f64); // seconds since epoch in UTC
+
+impl From<Timestamp> for f64 {
+    fn from(t: Timestamp) -> Self {
+        t.0
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
